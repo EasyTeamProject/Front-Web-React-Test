e4af27e23289a1075f3254c174e218aa5959129c
Florian Richard <frichard@Asset-group-opera.local>
2019-05-23 17:44:38 +0200

    login and signup components

---
+++ src/components/Login.js
@@ -0,0 +1,65 @@
+import '../css/login.css';
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+
+export class Login extends Component {
+    state = {
+        usernameSignIn : '',
+        passwordSignIn : '',
+    }
+
+    
+
+    //Sign In
+    signInButtonHandler = () => {
+        var infos = JSON.stringify(this.state);
+        console.log(infos);
+    }
+
+    //Handle fields change
+    handleChange = input => e => {
+        this.setState({[input]: e.target.value});
+    }
+
+    render() {
+        // const { state } = this.state;
+        const { usernameSignIn, passwordSignIn } = this.state;
+        const values = { usernameSignIn, passwordSignIn };
+
+        
+
+        return (
+            <MuiThemeProvider>
+                <React.Fragment>
+                    <div className="loginFields">
+                        <TextField
+                            hintText="Enter your User Name"
+                            floatingLabelText="User Name"
+                            onChange={this.handleChange('usernameSignIn')}
+                            defaultValue={values.usernameSignIn}
+                        />
+                        <br/>
+                        <TextField
+                            type="password"
+                            hintText="Enter your Password"
+                            floatingLabelText="Password"
+                            onChange={this.handleChange('passwordSignIn')}
+                            defaultValue={values.passwordSignIn}
+                        />
+                        <br/>
+                        <RaisedButton
+                            className="loginButton"
+                            label="Sign In"
+                            primary={true}
+                            onClick={this.signInButtonHandler}
+                        />
+                    </div>
+                </React.Fragment>
+            </MuiThemeProvider>
+        )
+    }
+}
+
+export default Login
---
+++ src/components/SignUp.js
@@ -0,0 +1,87 @@
+import '../css/signUp.css';
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+
+export class SignUp extends Component {
+    state = {
+        familyName : '',
+        name : '',
+        email : '',
+        passwordSignUp : '',
+        confirmPassword : ''
+    }
+
+    //Handle fields change
+    handleChange = input => e => {
+        this.setState({[input]: e.target.value});
+    }
+
+    //Sign Up
+    signUpButtonHandler = () => {
+        var infos = JSON.stringify(this.state);
+        console.log(infos);
+    }
+
+    render() {
+        const { familyName, name, email, passwordSignUp, confirmPassword } = this.state;
+        const values = { familyName, name, email, passwordSignUp, confirmPassword };
+
+
+        return (
+            <MuiThemeProvider>
+                <React.Fragment>
+                    <div className="signUpFields">
+                        <TextField
+                            hintText="Enter your Family Name"
+                            floatingLabelText="Family Name"
+                            onChange={this.handleChange('familyName')}
+                            defaultValue={values.familyName}
+                        />
+                        <br/>
+                        <TextField
+                            hintText="Enter your Name"
+                            floatingLabelText="Name"
+                            onChange={this.handleChange('name')}
+                            defaultValue={values.name}
+                        />
+                        <br/>
+                        <TextField
+                            hintText="Enter your Email Address"
+                            floatingLabelText="Mail"
+                            onChange={this.handleChange('email')}
+                            defaultValue={values.email}
+                            type="email"
+                        />
+                        <br/>
+                        <TextField
+                            type="password"
+                            hintText="Enter your Password"
+                            floatingLabelText="Password"
+                            onChange={this.handleChange('passwordSignUp')}
+                            defaultValue={values.passwordSignUn}
+                        />
+                        <br/>
+                        <TextField
+                            type="password"
+                            hintText="Confirm your Password"
+                            floatingLabelText="Password"
+                            onChange={this.handleChange('confirmPassword')}
+                            defaultValue={values.confirmPassword}
+                        />
+                        <br/>
+                        <RaisedButton
+                            className="signUpButton"
+                            label="Sign Up"
+                            primary={true}
+                            onClick={this.signUpButtonHandler}
+                        />
+                    </div>
+                </React.Fragment>
+            </MuiThemeProvider>
+        )
+    }
+}
+
+export default SignUp
